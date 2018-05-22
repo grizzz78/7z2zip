6587176aff6bae96cd15b1d69db02c70a32b1364
grizzz78 <mail@griz.de>
2018-05-22 09:34:27 +0200

    Update 7z2zip.sh

--- 7z2zip.sh
+++ 7z2zip.sh
@@ -6,9 +6,12 @@ do
   zipname=`echo "${i}" | sed -e "s/\.7z/\.zip/g"`
   mkdir tmp
   cd tmp
+  printf "${i} ... decompress "
   7za e "../${i}" >> ../log.txt
+  printf "... compress to zip ... "
   zip -r "../output/${zipname}" . >> ../log.txt
   cd ..
-  rm -r tmp
-  echo "${i} -> ${zipname}"
+  rm -fr tmp
+  printf "ok\r\n";
 done
+printf "All files written to directory output\r\n"
